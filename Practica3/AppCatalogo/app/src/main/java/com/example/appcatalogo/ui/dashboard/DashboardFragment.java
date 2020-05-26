248b44dc46a5af8990f4f2cc117afd546391e4d9
Dunspa <jlgallego99@correo.ugr.es>
2020-05-26 18:39:23 +0200

    Conexion con API REST

--- Practica3/AppCatalogo/app/src/main/java/com/example/appcatalogo/ui/dashboard/DashboardFragment.java
+++ Practica3/AppCatalogo/app/src/main/java/com/example/appcatalogo/ui/dashboard/DashboardFragment.java
@@ -2,6 +2,8 @@ package com.example.appcatalogo.ui.dashboard;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
@@ -12,6 +14,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,26 +23,100 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.appcatalogo.R;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.LogRecord;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+// Representa cada uno de los datos que vamos a estar recibiendo por medio de la API REST
+class Post {
+    private String name;
+    private String desc;
+    private double precio;
+    private String link;
+    private String img;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
+
+// Interfaz para gestionar cada petición a la API REST
+interface PostService {
+    String API_ROUTE = "/PracticaFinal/api/hello/getAllGames";    // URI donde se realizará la petición
+
+    @GET(API_ROUTE)
+    Call< List<Post> > getPost();
+}
+
 
 class CustomList extends ArrayAdapter<String> {
 
     private final Activity context;
-    private final ArrayList<String> texto;
-    private final ArrayList<String> descripcion;
-    private final ArrayList<Double> precio;
-    private final ArrayList<Integer> idImagen;
-    public CustomList(Activity context, ArrayList<String> texto, ArrayList<String> descripcion, ArrayList<Double> precio, ArrayList<Integer> idImagen) {
+    private ArrayList<String> texto;
+    private ArrayList<String> descripcion;
+    private ArrayList<Double> precio;
+    private ArrayList<String> imagen;
+    private ArrayList<String> web;
+
+    public CustomList(Activity context, ArrayList<String> texto, ArrayList<String> descripcion, ArrayList<Double> precio, ArrayList<String> imagen, ArrayList<String> web) {
         super(context, R.layout.elemento_catalogo, texto);
         this.context = context;
         this.texto = texto;
         this.descripcion = descripcion;
         this.precio = precio;
-        this.idImagen = idImagen;
+        this.imagen = imagen;
+        this.web = web;
     }
 
     @Override
@@ -52,16 +130,48 @@ class CustomList extends ArrayAdapter<String> {
 
         txtTitle.setText(texto.get(position));
         descrip.setText(descripcion.get(position));
-        pre.setText("Precio: " + Double.toString(precio.get(position)) + "€");
-        imageView.setImageResource(idImagen.get(position));
+        pre.setText("Precio: " + Double.toString(precio.get(position)) + "€");;
+        Picasso.get().load(imagen.get(position)).into(imageView);
 
         return rowView;
+    }
+
+    public String getWeb(int position) {
+        return web.get(position);
+    }
+
+    public void getPosts() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8084")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        PostService postService = retrofit.create(PostService.class);
+        Call< List<Post> > call = postService.getPost();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                for(Post post : response.body()) {
+                    texto.add(post.getName());
+                    descripcion.add(post.getDesc());
+                    precio.add(post.getPrecio());
+                    imagen.add(post.getImg());
+                    web.add(post.getLink());
+                }
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+            }
+        });
     }
 }
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private CustomList adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -71,62 +181,17 @@ public class DashboardFragment extends Fragment {
 
         ListView list;
         final ArrayList<String> nombre = new ArrayList<>();
-        nombre.add("Nombre1");
-        nombre.add("Nombre2");
-        nombre.add("Nombre3");
-        nombre.add("Nombre4");
-        nombre.add("Nombre5");
-        nombre.add("Nombre6");
-        nombre.add("Nombre7");
-
         final ArrayList<String> descripcion = new ArrayList<>();
-        descripcion.add("Una descripción explicando algo");
-        descripcion.add("Una descripción explicando algo");
-        descripcion.add("Una descripción explicando algo");
-        descripcion.add("Una descripción explicando algo");
-        descripcion.add("Una descripción explicando algo");
-        descripcion.add("Una descripción explicando algo");
-        descripcion.add("Una descripción explicando algo");
-
         final ArrayList<Double> precio = new ArrayList<>();
-        precio.add(30.12);
-        precio.add(2.60);
-        precio.add(1.54);
-        precio.add(20.01);
-        precio.add(33.01);
-        precio.add(12.12);
-        precio.add(300.12);
+        final ArrayList<String> imagen = new ArrayList<>();
+        final ArrayList<String> web = new ArrayList<>();
 
-        final ArrayList<Integer> imageId = new ArrayList<>();
-        imageId.add(R.drawable.image);
-        imageId.add(R.drawable.image);
-        imageId.add(R.drawable.image);
-        imageId.add(R.drawable.image);
-        imageId.add(R.drawable.image);
-        imageId.add(R.drawable.image);
-        imageId.add(R.drawable.image);
-
-        final CustomList adapter = new CustomList(getActivity(), nombre, descripcion, precio, imageId);
+        adapter = new CustomList(getActivity(), nombre, descripcion, precio, imagen, web);
         list = (ListView) root.findViewById(R.id.catalogo);
         list.setAdapter(adapter);
 
-        EditText filtro = (EditText) root.findViewById(R.id.filtrobusqueda);
-        filtro.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                adapter.getFilter().filter(s);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        // Realizar petición a API REST
+        adapter.getPosts();
 
         // Actualizar lista
         /*final SwipeRefreshLayout actualizar = (SwipeRefreshLayout) root.findViewById(R.id.swiperefresh);
@@ -149,7 +214,7 @@ public class DashboardFragment extends Fragment {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Uri uri = Uri.parse("http://www.google.com");
+                Uri uri = Uri.parse(adapter.getWeb(i));
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
